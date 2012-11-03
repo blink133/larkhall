4c48c982a5c1147aae3768ad762b136ea1e66290
blink133 <disappointer@fromru.com>
2012-11-03 13:05:31 +0400

    fixed ip and port detection

--- web/app.py
+++ web/app.py
@@ -5,8 +5,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Le Flask running on %s:%s" % ( os.environ['IP'], os.environ['PORT'])
+    return "Le Flask running "
 
 if __name__ == "__main__":
-    app.run(host=os.environ['IP'], port=os.environ['PORT'])
-    
+    host = 'IP' in os.environ and os.environ['IP'] or '0.0.0.0';
+    port = 'PORT' in os.environ and int(os.environ['PORT']) or 7000;
+#    app.run(host=os.environ['IP'], port=os.environ['PORT'])
+    app.run(host=host, port=port)
